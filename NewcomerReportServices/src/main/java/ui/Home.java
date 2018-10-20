061ff16a28700b56590d1b2d27adcf419c6958ed
ThisIsAFakeName <brianchim1@gmail.com>
2018-10-19 23:14:04 -0400

    Made minor adjustments to login page and login CSS from javafx-poc branch, created blank Home, Register stages for connection after US1 is completed. Also pulled gitignore and maven build from US1.

---
+++ NewcomerReportServices/src/main/java/ui/Home.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Home extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+  	  Stage newStage = new Stage();
+  	  newStage.show();
+		
+	}
+
+}
---
+++ NewcomerReportServices/src/main/java/ui/Signup.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Signup extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Registration Form JavaFX Application");
+	    primaryStage.show();
+	}
+
+}
