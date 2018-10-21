88cf6e686af2bdc195d1e2163840909adc4bce71
jwmaggiezhou <jiawenmaggie.zhou@mail.utoronto.ca>
2018-10-21 14:57:15 -0400

    fix syntax

--- NewcomerReportServices/src/main/java/ui/Main.java
+++ NewcomerReportServices/src/main/java/ui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		new Signup().start(primaryStage);;
+		new Signup().start(primaryStage);
 		
 	}
 
