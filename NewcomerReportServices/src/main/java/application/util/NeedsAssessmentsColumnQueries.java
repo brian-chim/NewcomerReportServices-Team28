4489691bbf905b498e55835a01d54333c70781ee
ThisIsAFakeName <brianchim1@gmail.com>
2018-11-01 14:49:15 -0400

    Adding missed NARS file from last commit

---
+++ NewcomerReportServices/src/main/java/application/util/NeedsAssessmentsColumnQueries.java
@@ -0,0 +1,30 @@
+package application.util;
+
+public enum NeedsAssessmentsColumnQueries {
+	CLIENTBIRTHDATE("client_birth_dt", "Client Birth Date"),
+	SERVICELANGUAGE("assessment_language_id", "Language of Service"),
+	PREFLANGUAGE("preferred_official_language_id", "Preferred Language"),
+	INSTITUTIONTYPE("institution_type_id", "Institution Type"),
+	REFERREDBY("assessment_referral_id", "Referral Service"),
+	FINDEMPLOYMENTNEEDS("find_employment_needs_ind", "Needs Employment Finding Services"),
+	CANADIANINTENTION("intention_canadian_citizen_ind", "Intention to become Canadian Citizen"),
+	TRANSPORTATIONNEED("transporation_required_ind", "Transporation needed"),
+	TRANSLATIONNEED("translation_required_ind", "Translation needed"),
+	HOUSINGNEED("housing_accommodation_needs_ind", "Housing needed");
+
+	private String dbName;
+	private String uiName;
+
+	NeedsAssessmentsColumnQueries(String dbName, String uiName) {
+		this.dbName = dbName;
+		this.uiName = uiName;
+	}
+	
+	public String getDbName() {
+		return this.dbName;
+	}
+	
+	public String getUiName() {
+		return this.uiName;
+	}
+}
