86d57b8f31b0a40170f6f36085d15ae4add92941
ThisIsAFakeName <brianchim1@gmail.com>
2018-11-15 19:05:53 -0500

    Finished work on adding phone number formatter

--- NewcomerReportServices/src/main/java/application/util/Formatter.java
+++ NewcomerReportServices/src/main/java/application/util/Formatter.java
@@ -18,5 +18,25 @@ public class Formatter {
 		}
 	}
 
+	/**
+	 * Formats a given phone number into Canadian domestic style
+	 * @param input a string input of phone number
+	 * @return a valid and formatted Canadian phone number
+	 * @throws InvalidValueException if the input value is not valid to be formatted
+	 */
+	public static String formatPhoneNumber (String input) throws InvalidValueException {
+		// replace spaces by empty string
+		input = input.replaceAll("\\s",  "");
+		// replace all dashes with empty space
+		input = input.replaceAll("-", "");
+		input = input.replaceAll("\\(", "");
+		input = input.replaceAll("\\)", "");
+		// check if input is 9 numbers
+		if(input.matches("\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d")) {
+			return "(" + input.substring(0, 3) + ") " + input.substring(3, 6) + "-" + input.substring(6, input.length());
+		} else {
+			throw new InvalidValueException();
+		}
+	}
 
 }
